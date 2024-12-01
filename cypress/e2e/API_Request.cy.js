1ffaa1de2987a53af49bd84b55d8e92645b014a1
ManiDGM <manidgm1383@gmail.com>
2024-12-01 16:10:14 +0330

    project updated

--- cypress/e2e/API_Request.cy.js
+++ cypress/e2e/API_Request.cy.js
@@ -3,11 +3,102 @@ describe('template spec', () => {
     cy.visit('https://samin-sec.nadindev.ir/swagger/index.html');
   });
 
-  it('sends request with token', () => {
-      const token = Cypress.env('token');       //TODO: fix the token
+  it('sends request to further information question', () => {
+    const token = Cypress.env('API_TOKEN');
       cy.request({
         method: 'GET',
         url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/furtherinformationquestion',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to attachments', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/attachments',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to profile', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/profile',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to education', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/education',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to occupation', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/occupation',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to relation', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/relation',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to professional degree', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/professionaldegree',
+        headers: {
+          Authorization: `Bearer ${token}` 
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+  });
+
+  it('sends request to further information answer', () => {
+    const token = Cypress.env('API_TOKEN');
+      cy.request({
+        method: 'GET',
+        url: 'https://samin-sec.nadindev.ir/api/v1/realprofile/furtherinformationanswer',
         headers: {
           Authorization: `Bearer ${token}` 
         },
